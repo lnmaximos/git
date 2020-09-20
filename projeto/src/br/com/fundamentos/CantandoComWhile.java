fbe4a2a4806dc831d87de1768362a999044c3623
León Máximos Ramos <68385@PROWAY.TREINA>
2020-09-20 16:52:21 -0300

    exemplo do professor de cantando com while

--- projeto/src/br/com/fundamentos/CantandoComWhile.java
+++ projeto/src/br/com/fundamentos/CantandoComWhile.java
@@ -1,13 +1,34 @@
 package br.com.fundamentos;
 
 public class CantandoComWhile {
-	
+
 	public static void main(String[] args) {
-		
-		int patos = 5;
-		
-		while (patos > 0) {
-			System.out.println(patos + " patinhos foram passear\nAlém das montanhas para brincar\nA mamãe gritou: \"Quá, quá, quá, quá\"\nMas só " + --patos + " patinhos voltaram de lá\n");
+		int patinhos = 5;
+		while (patinhos != 0) {
+			
+			if (patinhos != 1) {
+				System.out.printf("%d patinhos foram passear\n", patinhos);
+				System.out.println("Além das montanhas");
+				System.out.println("Para brincar");
+				System.out.println("A mamãe gritou: Quá, quá, quá, quá");
+				System.out.printf("Mas só %d patinhos voltaram de lá.\n", --patinhos);
+				System.out.println();
+			} else {
+				System.out.printf("%d patinho foi passear\n", patinhos);
+				System.out.println("Além das montanhas");
+				System.out.println("Para brincar");
+				System.out.println("A mamãe gritou: Quá, quá, quá, quá");
+				System.out.println("Mas nenhum patinho voltou de lá ");
+				System.out.println();
+				patinhos--;
+			}
+
 		}
+		System.out.println("A mamãe patinha foi procurar");
+		System.out.println("Além das montanhas");
+		System.out.println("Na beira do mar");
+		System.out.println("A mamãe gritou: Quá, quá, quá, quá");
+		System.out.println("E os 5 patinhos voltaram de lá.");
 	}
+
 }
