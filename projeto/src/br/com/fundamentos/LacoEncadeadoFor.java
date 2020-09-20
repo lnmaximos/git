5cb30e34e8561cce39521ca0622da33cf9309397
León Máximos Ramos <68385@PROWAY.TREINA>
2020-09-20 18:55:38 -0300

    LacoEncadeadoFor

---
+++ projeto/src/br/com/fundamentos/LacoEncadeadoFor.java
@@ -0,0 +1,18 @@
+package br.com.fundamentos;
+
+public class LacoEncadeadoFor {
+	
+	public static void main(String[] args) {
+		
+		for(int linha = 1; linha <= 10; linha++) {
+			
+				for (int coluna = 1; coluna <= 10; coluna++) {
+					if(coluna > linha) {
+						break;
+					}
+					System.out.print("*");
+			}
+				System.out.println();
+		}
+	}
+}
